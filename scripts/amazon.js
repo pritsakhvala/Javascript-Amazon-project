d99db981e5fbea595851c5dc88e1231b40372df7
pritsakhvala <pritpatel1373@gmail.com>
2024-05-07 10:57:33 +0530

    use modules and import cart from cart.js

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,3 +1,5 @@
+import { cart } from "../data/cart.js";
+
 let productsHTML = "";
 //accumulating pattern because each time we get html and added in productshtml in content.
 products.forEach((product) => {
